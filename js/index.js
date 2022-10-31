6f41c6a38b3c55203065a26dd546754a04e1f7a8
Pablo-VE <pablovenegaselizondo@gmail.com>
2022-10-30 22:19:38 -0600

    tables page ready

--- js/index.js
+++ js/index.js
@@ -13,3 +13,19 @@ function outFunc() {
     var tooltip = document.getElementById("myTooltip");
     tooltip.innerHTML = "Copy to clipboard";
 }
+
+function copyCode(codeId, tooltipId) {
+    var copyText = document.getElementById(codeId);
+    navigator.clipboard.writeText(copyText.textContent);
+
+    var tooltip = document.getElementById(tooltipId);
+    tooltip.innerHTML = "Code copied";
+
+    // alert("Copied the text: " + copyText.textContent);
+    // console.log("hola mundo");
+}
+
+function cleanTooltip(id) {
+    var tooltip = document.getElementById(id);
+    tooltip.innerHTML = "Copy to clipboard";
+}
